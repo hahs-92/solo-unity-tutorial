1270ddbb4a75747402daaad4f3c00524d0f20b7c
unknown <alexandre_2420@outlook.com>
2022-12-31 17:19:39 -0500

    save save game functionality

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,16 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public string startScene;
+    public GameObject continueButton;
+    public string startScene, continueScene;
 
+
+    private void Start()
+    {
+        if(PlayerPrefs.HasKey(startScene + "_unlocked" ))
+        {
+            // si ya jugamos el juego podemos, continuar
+            // se habilita el button de continuar
+            continueButton.SetActive(true);
+        } else
+        {
+            continueButton.SetActive(false);
+        }
+    }
 
     public void StartGame()
     {
         SceneManager.LoadScene(startScene);
+        PlayerPrefs.DeleteAll();
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void ContinueGame()
+    {
+        SceneManager.LoadScene(continueScene);
     }
 }
